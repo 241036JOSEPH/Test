70c3e64e9390cb42ad6861d791a5bae1636eab1f
241036JOSEPH <241036@hkis.edu.hk>
2022-05-25 15:14:40 +0800

    new commit
    desc

--- ClassTest.java
+++ ClassTest.java
@@ -1,9 +1,11 @@
 package Test;
 
+//update: 1
+
 public class ClassTest {
     public static void main(String[] args) {
 
-        System.out.print("TEST" );
-        
+        System.out.print("TEST");
+
     }
 }
